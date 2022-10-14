5bb7c39814f571fb89b6bfeaa54c90c9d3378270
mai-sami <mai.jaber@hotmail.com>
2022-10-14 15:45:43 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'translation-demo';
+export class AppComponent implements OnInit {
+  languages=[
+    {code:'en', dir:'ltr',name:"english"},
+    {code:'ar', dir:'rtl',name:"arabic"}
+  ]
+  currentLanguageCode='en'
+   lang:any={}
+  constructor(private translate: TranslateService) {
+   }
+  ngOnInit(): void {
+    this.onLanguageSelected(this.currentLanguageCode)
+   }
+  onLanguageSelected(langCode:any){
+     this.currentLanguageCode=langCode
+    this.lang=this.languages.find(l=>l.code==this.currentLanguageCode)
+    this.translate.setDefaultLang(this.lang.code)
+    this.translate.use(this.lang.code)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { ChildComponent } from './child/child.component';
+function HttpLoaderFactorys(http: HttpClient) {
+  return new TranslateHttpLoader(http,"./assets/i18n/",".json");
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+    TranslateModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactorys,
+          deps: [HttpClient]
+      }
+  })
   ],
   providers: [],
   bootstrap: [AppComponent]
